90978625b371163082c6bcfa32e6b27aa2210dd4
Mangiang <mangiang@orange.fr>
2020-01-09 00:15:41 -0500

    Add logo + rename to jsx

--- cpu/shirito-ind-front/frontend/src/App.jsx
+++ cpu/shirito-ind-front/frontend/src/App.jsx
@@ -6,6 +6,7 @@ import init from "./editor";
 function App() {
   return (
     <div className="App">
+      <img src="logo.png"/>
       <div style={{ textAlign: "left", width: "100vw", height: "70vh" }}>
         <div ref={el => init(el)} />
       </div>
